16d0c40cab9f0be76711e7ea775b05567ba788e5
uriel djantou  fanja <137457721+Rileystra@users.noreply.github.com>
2026-06-16 20:26:51 +0000

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "stra23"
-    key = "server_name/statefile"
-    region = "ap-south-1"
+    key    = "ansible-project/statefile"
+    region = "eu-west-2"
   }
-}  
+}
